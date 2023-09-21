8e5fc9e4969c7b9cc2a3cb789ad0f58ffa53c91b
AbashinAE <a.abashin@list.ru>
2023-09-21 23:21:08 +0300

    Add Toys and Shop

--- src/main/java/Toy.java
+++ src/main/java/Toy.java
@@ -1,41 +1,31 @@
 public class Toy {
     private String name;
-
     private int count;
-
     private Integer weight;
-
     public Toy(String name, int count, int weight) {
         this.name = name;
         this.count = count;
         this.weight = weight;
     }
-
     public int getCount() {
         return count;
     }
-
     public void setCount(int count) {
         this.count = count;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     @Override
     public String toString() {
         return "Toy: name = " + name + ", count = " + count + ", weight = " + weight;
     }
-
     public Integer getDefaultWeight() {
         return weight;
     }
-
     public Toy reduceWeight() {
         weight--;
         return this;
